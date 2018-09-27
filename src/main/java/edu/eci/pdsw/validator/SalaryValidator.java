c14a7bb03880b0d714e466bb1de98e7d5eb9651b
Sergio Ruiz <sergyoruiz97@gmail.com>
2018-09-26 20:36:41 -0500

    Fix test

--- src/main/java/edu/eci/pdsw/validator/SalaryValidator.java
+++ src/main/java/edu/eci/pdsw/validator/SalaryValidator.java
@@ -3,7 +3,6 @@ package edu.eci.pdsw.validator;
 import java.util.Optional;
 
 import edu.eci.pdsw.model.Employee;
-import edu.eci.pdsw.model.SocialSecurityType;
 
 /**
  * Utility class to validate an employee's salary
@@ -15,27 +14,15 @@ public class SalaryValidator implements EmployeeValidator {
 	 */
 	public Optional<ErrorType> validate(Employee employee) {
 		
-		if (employee.getPersonId() < 1000 || employee.getPersonId() > 100000) {
-			return Optional.of(ErrorType.INVALID_ID);
-		}
 		if (employee.getSalary() < 100 || employee.getSalary() > 50000) {
 			return Optional.of(ErrorType.INVALID_SALARY);
 		}
-		if (employee.getSocialSecurityType() == SocialSecurityType.SISBEN) {
-			if (employee.getSalary() >= 1500) {
-				return Optional.of(ErrorType.INVALID_SISBEN_AFFILIATION);
-			}
+		if (employee.getSalary() > 1500) {
+			return Optional.of(ErrorType.INVALID_SISBEN_AFFILIATION);
 		}
-		if (employee.getSocialSecurityType() == SocialSecurityType.EPS) {
-			if (employee.getSalary() < 1500 || employee.getSalary() >= 10000) {
-				return Optional.of(ErrorType.INVALID_EPS_AFFILIATION);
-			}
-		}
-		if (employee.getSocialSecurityType() == SocialSecurityType.PREPAID) {
-			if (employee.getSalary() < 10000) {
-				return Optional.of(ErrorType.INVALID_PREPAID_AFFILIATION);
-			}
-		}
-		return Optional.empty();
+		if (employee.getSalary() <= 1500 && employee.getSalary() >= 10000) {
+			return Optional.of(ErrorType.INVALID_EPS_AFFILIATION);
+		}else
+			return Optional.of(ErrorType.INVALID_PREPAID_AFFILIATION);
 	}
 }
--- src/test/java/edu/eci/pdsw/validator/SalaryValidatorTest.java
+++ src/test/java/edu/eci/pdsw/validator/SalaryValidatorTest.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.junit.Test;
 
-import edu.eci.pdsw.model.SocialSecurityType;
 import generators.EmployeeGenerator;
 
 /**
@@ -29,22 +28,20 @@ public class SalaryValidatorTest {
 		.check(employee -> {
 			Optional<ErrorType> optional;
 			optional = validator.validate(employee);
-			if (!(1000 <= employee.getPersonId() && employee.getPersonId() <= 100000 )) {
+			if (employee.getPersonId() < 1000 || employee.getPersonId() > 100000) {
 				return optional.equals(Optional.of(ErrorType.INVALID_ID));
 			}
-			if (!(100 <= employee.getSalary() && employee.getSalary() >= 50000)) {
+			if (employee.getSalary() < 100 || employee.getSalary() > 50000) {
 				return optional.equals(Optional.of(ErrorType.INVALID_SALARY));
 			}
-			if (employee.getSocialSecurityType() == SocialSecurityType.SISBEN && employee.getSalary() >= 1500) {
+			if (employee.getSalary() > 1500) {
 				return optional.equals(Optional.of(ErrorType.INVALID_SISBEN_AFFILIATION));
 			}
-			if (employee.getSocialSecurityType() == SocialSecurityType.EPS && (employee.getSalary() < 1500 || employee.getSalary() >= 10000)) {
-					return optional.equals(Optional.of(ErrorType.INVALID_EPS_AFFILIATION));
-			}
-			if (employee.getSocialSecurityType() == SocialSecurityType.PREPAID && employee.getSalary() < 10000) {
-					return optional.equals(Optional.of(ErrorType.INVALID_PREPAID_AFFILIATION));
-			}			
-			return optional == null;
+			if (employee.getSalary() <= 1500 && employee.getSalary() >= 10000) {
+				return optional.equals(Optional.of(ErrorType.INVALID_EPS_AFFILIATION));
+			}else
+				return optional.equals(Optional.of(ErrorType.INVALID_PREPAID_AFFILIATION));
+				
 		});
 	}
 }	
